20d5bc59dd7d2497831cb57ff5d8600e94472f21
Malibongwe <malib2027@gmail.com>
2024-05-25 05:43:06 +0200

    setup node-postgres

---
+++ db/index.js
@@ -0,0 +1,37 @@
+import pg from 'pg';
+const { Pool } = pg;
+
+const pool = new Pool();
+
+export const query = async (text, params) => {
+  const start = Date.now();
+  const res = await pool.query(text, params);
+  const duration = Date.now() - start;
+  console.log('executed query', { text, duration, rows: res.rowCount });
+  return res;
+};
+
+export const getClient = async () => {
+  const client = await pool.connect();
+  const query = client.query;
+  const release = client.release;
+
+  const timeout = setTimeout(() => {
+    console.error('A client has been checked out for more than 5 seconds!');
+    console.error(`The last executed query on this client was: ${client.lastQuery}`);
+  }, 5000);
+
+  client.query = (...args) => {
+    client.lastQuery = args;
+    return query.apply(client, args);
+  };
+
+  client.release = () => {
+    clearTimeout(timeout);
+    client.query = query;
+    client.release = release;
+    return release.apply(client);
+  };
+
+  return client;
+};
